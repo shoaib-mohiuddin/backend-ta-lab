6962abade89ed31089373cdda72f59c6bacbdd92
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2022-08-17 18:23:52 +0530

    Updates github role

--- role.tf
+++ role.tf
@@ -24,11 +24,7 @@ data "aws_iam_policy_document" "github_assume_role_policy" {
             variable = "token.actions.githubusercontent.com:sub"
             values = ["repo:shoaib-mohiuddin/*:*"]
         }
-        condition {
-            test = "StringLike"
-            variable = "token.actions.githubusercontent.com:sub"
-            values = ["repo:silpapriyadarshini/*:*"]
-        }
+        
     }
 }
 
